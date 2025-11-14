cdea8f26cf91f0a3f3095714587b2977a6d03c52
Olle <ollenglund@outlook.com>
2025-11-14 13:56:37 +0100

    upd

--- app.py
+++ app.py
@@ -45,29 +45,34 @@ fig = go.Figure(data=[
 ])
 
 fig.update_layout(
-    title=f"                 {ticker}, {date}",
+    title={
+        "text": f"{ticker}, {date}",
+        "font": {"size": 28}   # adjust title size here
+    },
     xaxis_title="Time",
     yaxis_title="Price",
     xaxis_rangeslider_visible=False
 )
 
+fig.update_layout(
+    xaxis_title="Time",
+    yaxis_title="Price",
+    xaxis_title_font={"size": 20},
+    yaxis_title_font={"size": 20},
+)
+
 # Show in Streamlit
 st.plotly_chart(fig, use_container_width=True)
 
-st.write("Ticker:", w['ticker'].iloc[0])
-st.write("Date:", w['t'].iloc[0].date())
+st.write("Ticker:", ticker)
+st.write("Date:", date)
 st.write("Time of first candle:", w['t'].iloc[0])
 st.write("Time of signal candle:", w['t'].iloc[21])
-
-
 
 sample_input = X_samples[selected_idx]
 sample_input = X_samples[selected_idx].reshape(1, -1) # shape (1, n_features) for tree models
 
-# Show input and label
-st.write("Selected sample features:", sample_input)
 st.write("True label:", true_label)
-
 # Make prediction
 prediction = rf_model.predict_proba(sample_input)
 st.write("Prediction:", prediction[0, 1])
