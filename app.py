599fb850135f87393ff9fb2f14bf61243a4ebd54
Olle <ollenglund@outlook.com>
2025-11-14 13:26:40 +0100

    upd

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ for i in range(5):
 
 
 st.write("Ticker of chosen window:", sample_windows[selected_idx]['ticker'].iloc[0])
-st.write("Date of chosen window:", sample_windows[selected_idx]['t'].iloc[0].day)
+st.write("Date of chosen window:", sample_windows[selected_idx]['t'].iloc[0].date())
 st.write("Time of first candle:", sample_windows[selected_idx]['t'].iloc[0])
 st.write("Time of signal candle:", sample_windows[selected_idx]['t'].iloc[21])
 
