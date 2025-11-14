d784a4a96fb5ce3859f4b46f290084e6e90224c3
Olle <ollenglund@outlook.com>
2025-11-14 13:31:42 +0100

    upd

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ X_samples = np.load("sample_data/X_flat_test.npy")
 y_samples = np.load("sample_data/y_binary_test.npy")
 
 # Let user pick a sample
-selected_idx = 1 - st.selectbox("Pick a sample window", list(range(1, len(X_samples) + 1)))
+selected_idx = 0 - st.selectbox("Pick a sample window", list(range(1, len(X_samples) + 1)))
 sample_input = X_samples[selected_idx]
 true_label = y_samples[selected_idx]
 
@@ -30,15 +30,8 @@ st.write("Date:", sample_windows[selected_idx]['t'].iloc[0].date())
 st.write("Time of first candle:", sample_windows[selected_idx]['t'].iloc[0])
 st.write("Time of signal candle:", sample_windows[selected_idx]['t'].iloc[21])
 
-
-st.write("Shape of X_samples:", X_samples.shape)
-st.write("Shape of y_samples:", y_samples.shape)
-
-
 sample_input = X_samples[selected_idx]
-st.write("Shape before:", sample_input.shape)
-sample_input = X_samples[selected_idx].reshape(1, -1)
-st.write("Shape after:", sample_input.shape)
+sample_input = X_samples[selected_idx].reshape(1, -1) # shape (1, n_features) for tree models
 
 # Show input and label
 st.write("Selected sample features:", sample_input)
