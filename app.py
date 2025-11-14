b8f5d3eabdc8c8fd47aa2cd27d30f6af4c11c612
Olle <ollenglund@outlook.com>
2025-11-14 14:18:58 +0100

    upd

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ date = df['t'].iloc[0].date()
 
 st.write("Chosen ticker:", ticker)
 st.write("Date:", date)
-st.write("Time of signal candle:",\\
+st.write("Time of signal candle:",\
     signal_time.time().replace(second=0, microsecond=0))  # keeps green, HH:MM
 
 fig = go.Figure(data=[
