3cc87363f1dd0dbbbeaa600a52c3ae22a6180847
Olle <ollenglund@outlook.com>
2025-11-14 15:48:50 +0100

    upd

--- app.py
+++ app.py
@@ -73,8 +73,7 @@ if isinstance(selected_option, int):
     # Select model
     options_models = ["Select a model..."] + list(models.keys())
     selected_model_name = st.selectbox("", options_models)
-    selected_model = models[selected_model_name]
-    st.write(selected_model)
+    st.write(selected_model_name)
     if selected_model_name != "Select a model...":
         selected_model = models[selected_model_name]
         st.write(f"Selected model: {selected_model_name}")
